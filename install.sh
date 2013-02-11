ec62c77cfddb230f6176f7fd73fa1c7198de72fb
alexdaly <alexdaly@gmail.com>
2013-02-10 22:01:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,36 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "I don't know what I am doing!!!"
+chmod 777 install.sh
 
-# What Linux distro are we on?
-OS= sed -n 's/^NAME=//p' /etc/*-release
-echo $OS
 
-sudo apt-get install gcc -y
-# yum install gcc -y
+sudo mkdir /usr/B2G
+cd B2G
 
-exit 0
+
+echo "export CC=`which gcc-4.6`" > B2G/.userconfig
+echo "export CXX=`which g++-4.6`" >> B2G/.userconfig
+
+#prereqs
+sudo apt-get install autoconf2.13 bison bzip2 ccache curl flex gawk gcc-4.6 g++ 4.6 g++-multilib 4.6 git ia32-libs lib32ncurses5-dev lib32z1-dev libgl1-mesa-dev libx11-dev make zip -y
+
+
+#symlinks
+sudo ln -s /usr/lib/i386-linux-gnu/libX11.so.6 /usr/lib/i386-linux-gnu/libX11.so
+
+sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
+
+#clone
+git clone git://github.com/mozilla-b2g/B2G.git 
+
+
+#configure emulator
+ 
+
+echo 'Name user@user.com' | ./config.sh emulator
+
+#build
+./build.sh
+
+
 
 
